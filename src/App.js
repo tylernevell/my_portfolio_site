d245175a871e0a7e75865b2efa226d48409b1dd9
tylernevell <tylernevell@gmail.com>
2020-07-26 14:25:00 -0700

    fixed routing for pages: About and Contact.

--- src/App.js
+++ src/App.js
@@ -55,8 +55,8 @@ class App extends React.Component {
                     </Navbar>
 
                     <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />}/>
-                    <Route path="/" exact render={() => <AboutPage title={this.state.about.title} />}/>
-                    <Route path="/" exact render={() => <ContactPage title={this.state.contact.title} />}/>
+                    <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />}/>
+                    <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />}/>
                     <Footer/>
 
                 </Container>
