5e8d5f1ea8530a6efb3cbdb8ecbfbe6d86f9a000
tylernevell <tylernevell@gmail.com>
2020-07-26 13:00:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Tyler Nevell',
+            headerLinks: [
+                {title: 'Home', path: '/'},
+                {title: 'About', path: '/about'},
+                {title: 'Contact', path: '/contact'},
+            ],
+            home: {
+                title: 'Iterate, Improve',
+                subTitle: 'The Portfolio of Tyler Nevell',
+                text: 'Feel free to explore my projects below.'
+            },
+            about: {
+                title: 'About Me'
+            },
+            contact: {
+                title: 'Let\'s Talk'
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>Hello from React</div>
+        );
+    }
+
 }
 
 export default App;
