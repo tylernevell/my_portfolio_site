2f8665e94bbfb02acab11c599910d2f9fef401ed
tylernevell <tylernevell@gmail.com>
2020-07-26 16:05:58 -0700

    react spring functionality added.

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import CardInfo from '../components/CardInfo';
 function Card(props) {
 
     return(
-        <div className="d-inline-block t-card">
+        <div className="d-inline-block t-card" onClick={(e) => props.click(props.item)}>
             <img className="t-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
         </div>
